793791d0ccd79f54402732df0773a160829f4197
Max Persson <max@maxpersson.com>
2013-01-22 13:30:25 +0100

    Add license banner and change name of test service

--- tests/test_salut.py
+++ tests/test_salut.py
@@ -1,3 +1,18 @@
+# Copyright 2012 Loop Lab
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import unittest
 from mock import MagicMock
 
@@ -5,7 +20,7 @@ import socket
 import gevent
 import gevent.socket
 
-from otis.common.salut import Announcer, Browser
+from salut import Announcer, Browser
 
 
 class TestSalut(unittest.TestCase):
@@ -16,7 +31,7 @@ class TestSalut(unittest.TestCase):
         pass
 
     def test_announce(self):
-        announcer = Announcer('Test', '_otis_test._tcp', 9999)
+        announcer = Announcer('Test', '_salut_test._tcp', 9999)
 
         while not announcer.announced:
             gevent.sleep(0.05)
@@ -26,22 +41,22 @@ class TestSalut(unittest.TestCase):
     def test_announce_registered_callback(self):
         callback = MagicMock()
         announcer = Announcer(
-            'Test', '_otis_test._tcp', 9999, callback.registered)
+            'Test', '_salut_test._tcp', 9999, callback.registered)
 
         while not announcer.announced:
             gevent.sleep(0.05)
         callback.registered.assert_called_once_with(
-            'local.', '_otis_test._tcp.', 'Test')
+            'local.', '_salut_test._tcp.', 'Test')
 
         announcer.stop()
 
     def test_browse(self):
-        announcer = Announcer('Test', '_otis_test._tcp', 9999)
+        announcer = Announcer('Test', '_salut_test._tcp', 9999)
         while not announcer.announced:
             gevent.sleep(0.05)
 
         browser = Browser(
-            'Test', '_otis_test._tcp')
+            'Test', '_salut_test._tcp')
 
         while not browser.resolved:
             gevent.sleep(0.05)
@@ -52,13 +67,13 @@ class TestSalut(unittest.TestCase):
     def test_browse_resolved_callback(self):
         ip = gevent.socket.gethostbyname(socket.gethostname())
         port = 9999
-        announcer = Announcer('Test', '_otis_test._tcp', port)
+        announcer = Announcer('Test', '_salut_test._tcp', port)
         while not announcer.announced:
             gevent.sleep(0.05)
 
         callback = MagicMock()
         browser = Browser(
-            'Test', '_otis_test._tcp',
+            'Test', '_salut_test._tcp',
             resolved_callback=callback.resolved)
 
         while not browser.resolved:
@@ -69,13 +84,13 @@ class TestSalut(unittest.TestCase):
         announcer.stop()
 
     def test_browse_unresolved_callback(self):
-        announcer = Announcer('Test', '_otis_test._tcp', 9999)
+        announcer = Announcer('Test', '_salut_test._tcp', 9999)
         while not announcer.announced:
             gevent.sleep(0.05)
 
         callback = MagicMock()
         browser = Browser(
-            'Test', '_otis_test._tcp',
+            'Test', '_salut_test._tcp',
             unresolved_callback=callback.unresolved)
 
         while not browser.resolved:
@@ -93,11 +108,11 @@ class TestSalut(unittest.TestCase):
         browser.stop()
 
     def test_unresolve_resolve(self):
-        announcer = Announcer('Test', '_otis_test._tcp', 9999)
+        announcer = Announcer('Test', '_salut_test._tcp', 9999)
         while not announcer.announced:
             gevent.sleep(0.05)
 
-        browser = Browser('Test', '_otis_test._tcp')
+        browser = Browser('Test', '_salut_test._tcp')
         while not browser.resolved:
             gevent.sleep(0.05)
 
@@ -109,7 +124,7 @@ class TestSalut(unittest.TestCase):
         while browser.resolved:
             gevent.sleep(0.05)
 
-        announcer = Announcer('Test', '_otis_test._tcp', 9999)
+        announcer = Announcer('Test', '_salut_test._tcp', 9999)
         while not announcer.announced:
             gevent.sleep(0.05)
         while not browser.resolved:
